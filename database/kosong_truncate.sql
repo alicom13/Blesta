96b9eac4bf3946145ac43b1be6a3206ca23d277f
Ali Musthofa <alfaonline13@gmail.com>
2021-06-21 15:37:50 +0700

    Create kosong_truncate.sql

---
+++ database/kosong_truncate.sql
@@ -0,0 +1,26 @@
+truncate support_replies;
+truncate support_tickets;
+truncate invoice_delivery;
+truncate invoice_lines;
+truncate invoices;
+truncate order_services;
+truncate orders;
+truncate service_fields;
+truncate services;
+truncate transaction_applied;
+truncate transactions;
+truncate log_account_access;
+truncate log_contacts;
+truncate log_emails;
+truncate log_gateways;
+truncate log_modules;
+/*truncate log_phpids;*/
+truncate log_services;
+truncate log_transactions;
+truncate log_users;
+truncate log_cron;
+/*
+|--------------------------------------------------------------------------
+| Sumber URL : https://www.blesta.com/forums/index.php?/topic/5030-clear-client-data-for-production-launch/
+|--------------------------------------------------------------------------
+*/
