c4a071744820cd78a42f9a08af3b5d93ea331300
Ali Musthofa <alfaonline13@gmail.com>
2025-03-21 01:46:32 +0700

    Create tunai.php

---
+++ components/gateways/nonmerchant/tunai/language/id_id/tunai.php
@@ -0,0 +1,7 @@
+<?php
+$lang['Tunai.name'] = 'Tunai Payment';
+$lang['Tunai.description'] = 'Memungkinkan Anda menampilkan instruksi khusus untuk menerima pembayaran';
+$lang['Tunai.instructions'] = 'Instructions';
+
+// Errors
+$lang['Tunai.!error.instructions.valid'] = 'You must enter some instructions.';
