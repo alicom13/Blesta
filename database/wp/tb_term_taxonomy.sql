b4f591aa32d1a6bdf91e588e8a6f05c09dcef606
Ali Musthofa <alfaonline13@gmail.com>
2025-07-02 20:08:11 +0700

    Create tb_term_taxonomy.sql

---
+++ database/wp/tb_term_taxonomy.sql
@@ -0,0 +1,12 @@
+TRUNCATE TABLE `tb_term_taxonomy`;
+INSERT INTO `tb_term_taxonomy` (`term_taxonomy_id`, `term_id`, `taxonomy`, `description`, `parent`, `count`) VALUES
+(1, 1, 'category', '', 0, 0),
+(2, 2, 'nav_menu', '', 0, 0),
+(3, 3, 'nav_menu', '', 0, 0),
+(4, 4, 'nav_menu', '', 0, 0),
+(5, 5, 'category', '', 0, 0),
+(6, 6, 'category', '', 0, 0),
+(7, 7, 'category', '', 0, 0),
+(8, 8, 'category', '', 0, 0),
+(9, 9, 'category', '', 0, 0),
+(10, 10, 'category', '', 0, 0);
