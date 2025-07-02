a65b66834d4d708f4a5065edbcc1058c4a74a4b8
Ali Musthofa <alfaonline13@gmail.com>
2025-07-02 20:06:13 +0700

    Create tb_terms.sql

---
+++ database/wp/tb_terms.sql
@@ -0,0 +1,13 @@
+TRUNCATE TABLE `tb_terms`;
+INSERT INTO `tb_terms` (`term_id`, `name`, `slug`, `term_group`) VALUES
+(1, 'Opini', 'opini', 0),
+(2, 'Menu 1', 'menu-1', 0),
+(3, 'Menu 2', 'menu-2', 0),
+(4, 'Menu 3', 'menu-3', 0),
+(5, 'Rubrik 1', 'rubrik-1', 0),
+(6, 'Rubrik 2', 'rubrik-2', 0),
+(7, 'Rubrik 3', 'rubrik-3', 0),
+(8, 'Rubrik 4', 'rubrik-4', 0),
+(9, 'Rubrik 5', 'rubrik-5', 0),
+(null, 'Rubrik 6', 'rubrik-6', 0);
+COMMIT;
