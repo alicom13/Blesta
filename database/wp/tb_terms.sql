939e1ca87803dfe858bac8fcc932283e938d7c26
Ali Musthofa <alfaonline13@gmail.com>
2025-07-17 07:09:24 +0700

    Update tb_terms.sql

--- database/wp/tb_terms.sql
+++ database/wp/tb_terms.sql
@@ -4,10 +4,10 @@ INSERT INTO `tb_terms` (`term_id`, `name`, `slug`, `term_group`) VALUES
 (2, 'Menu 1', 'menu-1', 0),
 (3, 'Menu 2', 'menu-2', 0),
 (4, 'Menu 3', 'menu-3', 0),
-(5, 'Rubrik 1', 'rubrik-1', 0),
-(6, 'Rubrik 2', 'rubrik-2', 0),
-(7, 'Rubrik 3', 'rubrik-3', 0),
-(8, 'Rubrik 4', 'rubrik-4', 0),
-(9, 'Rubrik 5', 'rubrik-5', 0),
-(null, 'Rubrik 6', 'rubrik-6', 0);
+(5, 'Teknologi', 'teknologi', 0),
+(6, 'Pendidikan', 'pendidikan', 0),
+(7, 'Keislaman', 'keislaman', 0),
+(8, 'Wordpress', 'wordpress', 0),
+(9, 'Codeigniter', 'codeigniter', 0),
+(null, 'OJS', 'ojs', 0);
 COMMIT;
